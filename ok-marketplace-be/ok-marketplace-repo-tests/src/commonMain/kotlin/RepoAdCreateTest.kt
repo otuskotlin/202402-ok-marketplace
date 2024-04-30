b45350fe07d43ac2697dc305777c04c7286a1d01
Sergey Okatov <sokatov@gmail.com>
2024-04-30 19:15:34 +0300

    m7l2 - Optimistic Locking

--- ok-marketplace-be/ok-marketplace-repo-tests/src/commonMain/kotlin/RepoAdCreateTest.kt
+++ ok-marketplace-be/ok-marketplace-repo-tests/src/commonMain/kotlin/RepoAdCreateTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.*
 
 abstract class RepoAdCreateTest {
     abstract val repo: IRepoAdInitializable
-    protected open val uuidNew = MkplAdId("10000000-0000-0000-0000-000000000001")
+    protected open val lockNew: MkplAdLock = MkplAdLock("20000000-0000-0000-0000-000000000002")
 
     private val createObj = MkplAd(
         title = "create object",
@@ -22,9 +22,8 @@ abstract class RepoAdCreateTest {
     @Test
     fun createSuccess() = runRepoTest {
         val result = repo.createAd(DbAdRequest(createObj))
-        val expected = createObj
         assertIs<DbAdResponseOk>(result)
-        assertEquals(uuidNew, result.data.id)
+        val expected = createObj.copy(id = result.data.id)
         assertEquals(expected.title, result.data.title)
         assertEquals(expected.description, result.data.description)
         assertEquals(expected.adType, result.data.adType)
