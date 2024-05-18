a93a5e35c9246fb81dced2476614502418962e29
Nickolay <phaeton03@gmail.com>
2024-05-18 19:06:05 +0300

    M5l1 spring (#19)
    * m2l1 coroutines
    
    (cherry picked from commit 1a1a67302b11ccbdc8c6c9fa415d6061a1dd8244)
    
    * m5l1 - Spring
    
    * m5l1 - Spring : fixes
    
    * m5l1 - Spring : postfixes
    
    * m5l1 - Spring
    
    * m5l1 - Spring fix
    
    * m5l1 - Spring deleted coroutines from root
    
    ---------
    
    Co-authored-by: Sergey Okatov <sokatov@gmail.com>

---
+++ ok-marketplace-be/ok-marketplace-stubs/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("build-kmp")
+}
+
+kotlin {
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+
+                implementation(project(":ok-marketplace-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
